bf27bc052c07d290207817b2a4ecf02dc83ad63e
kminnaaa <bexemen@gmail.com>
2025-04-21 21:08:38 +0900

    [Silver II] Title: 섬의 개수, Time: 60 ms, Memory: 32696 KB -BaekjoonHub

---
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.py
@@ -0,0 +1,32 @@
+# 백준 4963 dfs
+import sys
+sys.setrecursionlimit(10000)
+
+def dfs(x, y):
+    if x <= -1 or y <= -1 or x >= h or y >= w:
+        return False
+    if graph[x][y] == 1:
+        graph[x][y] = 0
+        dfs(x, y+1)
+        dfs(x, y-1)
+        dfs(x-1, y)
+        dfs(x+1, y)
+        dfs(x-1, y+1)
+        dfs(x-1, y-1)
+        dfs(x+1, y+1)
+        dfs(x+1, y-1)
+        return True
+    return False
+
+while True:
+    w, h = map(int, sys.stdin.readline().split())
+    if w == 0 and h == 0: break
+    graph = []
+    for i in range(h):
+        graph.append(list(map(int, sys.stdin.readline().split())))
+    result = 0
+    for i in range(h):
+        for j in range(w):
+            if dfs(i, j) == True:
+                result += 1
+    print(result)
