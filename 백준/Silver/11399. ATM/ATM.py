aee8ac5cdc26098ca3305aded7fb2d24a86961d4
kminnaaa <bexemen@gmail.com>
2025-04-29 09:50:43 +0900

    [Silver IV] Title: ATM, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -1,12 +1,11 @@
 # 백준 11399
 
 N = int(input())
-waiting = list(map(int, input().split()))
-
-waiting.sort()
-answer = 0
-
-for i in range(0, N):
-    answer += waiting[i] * (N-i)
-
-print(answer)
+arr = list(map(int,input().split()))
+arr.sort()
+result = 0
+time = 0
+for i in range(N):
+    time += arr[i]
+    result += time
+print(result)
