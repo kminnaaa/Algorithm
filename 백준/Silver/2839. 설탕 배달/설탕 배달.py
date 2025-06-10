43cb513ad34b04d26bd10de7b5825e207b21c1e0
kminnaaa <bexemen@gmail.com>
2025-06-10 14:18:38 +0900

    [Silver IV] Title: 설탕 배달, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,18 +1,18 @@
-# 백준 2839
-
 N = int(input())
+
+bag = [5, 3]
+
 count = 0
+while N > 0:    # 0이 되면 종료
+  if N % 5 == 0:
+    count += (N // 5)
+    break
+  elif N % 5 != 0 and N >= 3:
+    N -= 3
+    count += 1
+  elif N < 3:
+    count = -1
+    break
 
-while True:
-    if N % 5 == 0:
-        count += N // 5
-        break
-    elif N % 5 != 0:
-        N -= 3
-        count += 1
-
-    if N < 0:
-        count = -1
-        break
 
 print(count)
