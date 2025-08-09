2ab6d9587f9cfade2fd5e901ab2726564947899c
kminnaaa <bexemen@gmail.com>
2025-08-09 21:40:04 +0900

    [Gold V] Title: 토마토, Time: 1172 ms, Memory: 100496 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,3 +1,13 @@
+"""
+flag 안 쓰고 마지막 처리 하려면 ????
+
+1. exit() 즉시 프로그램 종료
+
+2.
+for-else
+  : break 없이 정상 종료된 경우에 else 블록 실행
+"""
+
 from collections import deque
 
 M, N = map(int, input().split())
@@ -18,17 +28,20 @@ def bfs():
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
+            # 범위 내 o, 안 익은 토마토 발견 > 일수 +1
             if 0 <= nx < N and 0 <= ny < M and tomato[nx][ny] == 0:
                 tomato[nx][ny] = tomato[x][y] + 1
-                queue.append((nx, ny))
+                queue.append((nx, ny))      # 다음 탐색 대상으로 저장
 
 bfs()
 
 res = 0
 for row in tomato:
+    # 아직 안 익은 거 있으면 -1
     if 0 in row:
         print(-1)
-        break
+        exit()
+    # 아닐 경우, max값으로 구해줌
     res = max(res, max(row))
-else:
-    print(res - 1)  # 익은 날이 1부터 시작하므로 -1 해야 실제 날짜
+
+print(res - 1)  # 익은 날이 1부터 시작하므로 -1 해야 실제 날짜
