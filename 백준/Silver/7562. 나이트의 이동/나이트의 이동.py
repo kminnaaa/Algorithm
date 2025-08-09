241835c2c01f9035c504a1f34f86f87dfb4b8dcd
kminnaaa <bexemen@gmail.com>
2025-08-09 20:51:54 +0900

    [Silver I] Title: 나이트의 이동, Time: 1192 ms, Memory: 34968 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
@@ -1,6 +1,9 @@
-# 백준 7562 bfs
-
+import sys
 from collections import deque
+input = sys.stdin.readline
+
+dx = [-2, -1, 1, 2, 2, 1, -1, -2]
+dy = [1, 2, 2, 1, -1, -2, -2, -1]
 
 def bfs(x, y):
     queue = deque()
@@ -8,25 +11,21 @@ def bfs(x, y):
 
     while queue:
         x, y = queue.popleft()
-        if x == destx and y == desty:
+        if x == dest_x and y == dest_y:
             return graph[x][y]
-
+        
         for i in range(8):
-            mx = x + dx[i]
-            my = y + dy[i]
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < I and 0 <= ny < I and graph[nx][ny] == 0:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx, ny))
 
-            if 0 <= mx < I and 0 <= my < I and graph[mx][my] == 0:
-                graph[mx][my] = graph[x][y] + 1
-                queue.append((mx, my))
+T = int(input())
 
-n = int(input())
-
-dx = [-2, -1, 1, 2, 2, 1, -1, -2]
-dy = [1, 2, 2, 1, -1, -2, -2, -1]
-
-for _ in range(n):
+for i in range(T):
     I = int(input())
-    graph = [[0]*I for i in range(I)]
-    x, y = map(int, input().split())
-    destx, desty = map(int, input().split())
-    print(bfs(x, y))
+    graph = [[0] * I for _ in range(I)]
+    cur_x, cur_y = map(int, input().split())
+    dest_x, dest_y = map(int, input().split())
+    print(bfs(cur_x, cur_y))
