34a7b39a3b8f482d2ad826d30b4f5af276247bb6
kminnaaa <bexemen@gmail.com>
2025-11-10 16:01:47 +0900

    [Silver V] Title: 색종이, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.py
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -1,16 +1,15 @@
-# 백준 2563
+board = [[0 for _ in range(100)] for _ in range(100)]
 
 N = int(input())
-arr = [[0] * 100 for _ in range(100)]
-answer = 0
+ans = 0
 
-for _ in range(N):
-    x, y = map(int, input().split())
-    for i in range(x, x+10):
-        for j in range(y, y+10):
-            arr[i][j] = 1
+for i in range(N):
+    w, h = map(int, input().split())
 
-for i in range(100):
-    answer += arr[i].count(1)
+    for j in range(10):
+        for k in range(10):
+            board[h + j][w + k] = 1
 
-print(answer)
+for sub in board:
+    ans += sub.count(1)
+print(ans)
