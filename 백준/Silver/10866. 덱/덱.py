9f1d8cfc5991659fa2bed3c6e3f82945522fd280
kminnaaa <bexemen@gmail.com>
2025-11-10 14:49:30 +0900

    [Silver IV] Title: 덱, Time: 64 ms, Memory: 34952 KB -BaekjoonHub

--- 백준/Silver/10866. 덱/덱.py
+++ 백준/Silver/10866. 덱/덱.py
@@ -1,35 +1,33 @@
-# 백준 10866
-
 import sys
 from collections import deque
 
 input = sys.stdin.readline
+
+
 N = int(input())
-Deque = deque()
+d = deque([])
 
 for i in range(N):
-    order = input().split()
+    c = input().split()
 
-    if order[0] == 'push_front':
-        Deque.appendleft(int(order[1]))
-    elif order[0] == 'push_back':
-        Deque.append(int(order[1]))
-    elif order[0] == 'pop_front':
-        if not Deque: print(-1)
-        else:
-            print( Deque.popleft())
-    elif order[0] == 'pop_back':
-        if not Deque: print(-1)
-        else:
-            print(Deque.pop())
-    elif order[0] == 'size':
-        print(len(Deque))
-    elif order[0] == 'empty':
-        if not Deque: print(1)
-        else: print(0)
-    elif order[0] == 'front':
-        if not Deque: print(-1)
-        else: print(Deque[0])
-    elif order[0] == 'back':
-        if not Deque: print(-1)
-        else: print(Deque[-1])
+    if c[0] == 'push_front':
+        d.insert(0, c[1])
+    elif c[0] == 'push_back':
+        d.append(c[1])
+    elif c[0] == 'pop_front':
+        if d: print(d.popleft())
+        else: print(-1)
+    elif c[0] == 'pop_back':
+        if d: print(d.pop())
+        else: print(-1)
+    elif c[0] == 'size':
+        print(len(d))
+    elif c[0] == 'empty':
+        if d: print(0)
+        else: print(1)
+    elif c[0] == 'front':
+        if d: print(d[0])
+        else: print(-1)
+    elif c[0] == 'back':
+        if d: print(d[-1])
+        else: print(-1)
