c5e4ab28bfc587f339eeb609978680fa19dbb4a1
Minha Kim <bexemen@gmail.com>
2026-02-11 11:23:52 +0900

    [Gold V] Title: 로봇 청소기, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
@@ -1,43 +1,37 @@
-# 백준 14503
-
-"""
-1. (r, c)가 청소되지 않은 경우(0) 현재 칸 청소
-
-2. 현재 칸의 주변 4칸(북, 동, 남, 서) 중 0이 없는 경우, 
-    # 북(0) r-1, c > 3
-    # 동(1) r, c+1 > 0
-    # 남(2) r+1, c > 1
-    # 서(3) r, c-1 > 2
-2-1. 바라보는 방향 유지하고 한칸 후진 후 1번
-
-3. 현재 칸 주변 4칸 중 0이 있는 경우, 반시계 방향으로 회전 (d+3)%4
-3-1. 바라보는 방향 기준, 앞쪽이 0일 경우 한 칸 전진 후 1번
-
-"""
+import sys
+input = sys.stdin.readline
 
 N, M = map(int, input().split())
 r, c, d = map(int, input().split())
-room = [list(map(int, input().split())) for _ in range(N)]
-dir = [[-1, 0], [0, 1], [1, 0], [0, -1]]
-answer = 0
 
+dy = [-1, 0, 1, 0]
+dx = [0, 1, 0, -1]
+
+room = []
+for i in range(N):
+  row = list(map(int, input().split()))
+  room.append(row)
+
+answer = 0
 while True:
-    flag = False
-    if room[r][c] == 0:
-        room[r][c] = 2
-        answer += 1
-    for i in range(4):
-        d = (d+3) % 4
-        nr, nc = r + dir[d][0], c + dir[d][1]
-        if room[nr][nc] == 0:
-            room[nr][nc] = 2
-            answer += 1
-            r, c = nr, nc
-            flag = True
-            break
-    if not flag:
-        if room[r-dir[d][0]][c-dir[d][1]] == 1:
-            print(answer)
-            break
-        else:
-            r, c = r - dir[d][0], c - dir[d][1]
+  if room[r][c] == 0:
+    room[r][c] = 2
+    answer += 1
+  
+  flag = 0
+  for i in range(4):
+    d = (d - 1) % 4
+    ny, nx = r + dy[d], c + dx[d]
+    if 0 <= ny < N and 0 <= nx < M and room[ny][nx] == 0:
+      r, c = ny, nx
+      flag = 1
+      break
+  if flag:
+    continue
+
+  by, bx = r - dy[d], c - dx[d]
+  if 0 <= by < N and 0 <= bx < M and room[by][bx] != 1:
+    r, c = by, bx
+  else:
+    print(answer)
+    break
