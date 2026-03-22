7d8869c22189c50897a4aa09720f2621fd039793
Minha Kim <bexemen@gmail.com>
2026-03-22 22:10:35 +0900

    [Silver I] Title: 신입 사원, Time: 4952 ms, Memory: 48060 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.py
+++ 백준/Silver/1946. 신입 사원/신입 사원.py
@@ -1,18 +1,19 @@
 # 백준 1946
 
 import sys
+input = sys.stdin.readline
 
-t = int(sys.stdin.readline())
+t = int(input())
 
 for i in range(t):
     
     arr = []
     answer = 1
 
-    N = int(sys.stdin.readline())
+    N = int(input())
     
     for j in range(N):
-        a, b = map(int, sys.stdin.readline().split())
+        a, b = map(int, input().split())
         arr.append([a, b])
         
     arr.sort()
