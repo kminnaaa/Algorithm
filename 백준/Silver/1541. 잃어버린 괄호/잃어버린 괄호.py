b9a3611f6eea949f637d98b066718903b850c20c
Minha Kim <bexemen@gmail.com>
2026-03-25 18:00:17 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -1,15 +1,16 @@
-num = input().split('-')
+tmp = input().split('-')
 
 arr = []
 
-for i in num:
-  sum = 0
-  add = i.split('+')
-  for j in add:
-    sum += int(j)
-  arr.append(sum)
+for n in tmp:
+    sum = 0
+    tmp = n.split('+')
+    for m in tmp:
+       sum += int(m)
+    arr.append(sum)
 
 for i in range(1, len(arr)):
-  arr[0] -= arr[i]
+   arr[0] -= arr[i]
 
 print(arr[0])
+        
