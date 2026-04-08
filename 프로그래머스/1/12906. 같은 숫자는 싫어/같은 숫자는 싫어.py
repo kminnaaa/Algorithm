c391e60c1fbadf7dea5823299d05841f2ed8e2dc
Minha Kim <bexemen@gmail.com>
2026-04-08 11:20:03 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 370.34 ms, Memory: 27.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -0,0 +1,12 @@
+def solution(arr):
+    answer = []
+    
+    for num in arr:
+        # 아직 없는 경우
+        if num not in answer:
+            answer.append(num)
+        # 이미 있지만, 연속 아닌 경우
+        elif num in answer and answer[len(answer) - 1] != num:
+            answer.append(num)
+    
+    return answer
