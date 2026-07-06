e0bd8316ad84682dcb1f5e084aee8ce2fb38ffea
Minha Kim <bexemen@gmail.com>
2026-07-06 20:04:42 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 68.68 ms, Memory: 25.8 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,23 +1,17 @@
 def solution(participant, completion):
+    
     answer = ''
-    # 한명만 완주 못함
-    # 이름 중복 가능 > 들어간 인원 중복 가능 > 들어간 인원이랑 나온 인원 비교
     
-    dict = {}
-    
-    for p in participant:
-        if p in dict:
-            dict[p] += 1
-        else:
-            dict[p] = 1
-    
-    for c in completion:
-        dict[c] -= 1
-    
-    for d, v in dict.items():
-        if v != 0:
-            answer = d
+    participant.sort()
+    completion.sort()
+
+    for i in range(len(participant) - 1):
+        if participant[i] != completion[i]:
+            answer += participant[i]
             break
-        
     
+    if answer == '':
+        answer += participant[-1]
+
+        
     return answer
